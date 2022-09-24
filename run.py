df09327f25885c67b0ba0a5afcb839f2b5bc7772
TareefIman <tareef.iman.azizi@gmail.com>
2022-09-25 00:32:05 +0800

    commit 4

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 from application import app
-
-if __name__ == "__main__":
-    app.run(debug = True)
+import  os
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
