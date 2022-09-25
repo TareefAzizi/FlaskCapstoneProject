ee2f2a8a837bf9347f43edfc72a179ed5a7ebc79
TareefIman <tareef.iman.azizi@gmail.com>
2022-09-25 19:09:31 +0800

    working commit
    pushing to github first

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
 from application import app
-import os
 
-
-if __name__ == '__main__':
-    app.run(debug=False, port=os.getenv("PORT", default=5000))
+if __name__ == "__main__":
+    app.run(debug = True)
