9238d8bd049f41431ba27caa959e9d161402bfd4
TareefIman <tareef.iman.azizi@gmail.com>
2022-09-25 19:13:42 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from application import app
+import os
 
-if __name__ == "__main__":
-    app.run(debug = True)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
